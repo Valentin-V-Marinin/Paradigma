f9ed47b445a4a9f2210679a7e1fd7c74c87a81b1
Valentin-V-Marinin <108931842+Valentin-V-Marinin@users.noreply.github.com>
2023-08-28 22:44:03 +0700

    Paradigma Seminar_3 HomeWork

---
+++ seminar_3/adapter/ElectricDevice.java
@@ -0,0 +1,16 @@
+package seminar_3.adapter;
+
+public abstract class ElectricDevice {
+
+    protected int voltage;
+
+    public boolean checkVoltage(int incomingVoltage, int ownVoltage){
+        if (incomingVoltage == ownVoltage){
+            return true;
+        } else {
+            return  false;
+        }
+    };
+
+}
+
---
+++ seminar_3/adapter/VoltageType.java
@@ -0,0 +1,5 @@
+package seminar_3.adapter;
+
+public enum VoltageType {
+    V110, V220
+}
